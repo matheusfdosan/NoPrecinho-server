006c96b67156cc8ac1a36d5df51eefae33d462ef
Matheus Faustino <104006185+matheusfdosan@users.noreply.github.com>
2025-05-24 19:29:21 -0300

    no more const

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -1,7 +1,7 @@
 const registerService = require("../services/registerService.js")
 
 const registerController = async (request, reply) => {
-  const { email, password } = request.body
+  let { email, password } = request.body
 
   const result = await registerService(email, password)
   reply.status(201).send(result)
