1718715d9824e9759ce2b4631126f65cd271ded3
Matheus Faustino <104006185+matheusfdosan@users.noreply.github.com>
2025-05-24 19:29:07 -0300

    no more const

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,7 @@
 const loginService = require("../services/loginService.js")
 
 const loginController = async (request, reply) => {
-  const { email, password } = request.body
+  let { email, password } = request.body
 
   const result = await loginService(email, password)
   reply.status(200).send(result)
